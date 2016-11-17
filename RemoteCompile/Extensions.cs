d27e89e2dee563fbfed4761a575b9580cce081a2
BenVlodgi <BenVlodgi@gmail.com>
2016-11-17 04:14:41 -0500

    Add VMF extensions
    Simplifies the task of checking for certain keyvalues in vmf, and
    getting a property with a specific key

---
+++ RemoteCompile/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using VMFParser;
+
+namespace RemoteCompile
+{
+    public static class Extensions
+    {
+        public static VProperty GetPropertyWithKey(this VBlock block, string key)
+        {
+            return block.Body.Where(node => node is VProperty && node.Name == key).Cast<VProperty>().FirstOrDefault();
+        }
+
+        public static bool HasPropertyWithKeyValue(this VBlock block, string key, string value)
+        {
+            return block.Body.Where(node => node is VProperty && node.Name == key && (node as VProperty).Value == value).Count() > 0;
+        }
+    }
+}
